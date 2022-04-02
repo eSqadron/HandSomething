a2756ae544d6825a2df8f0333efd37329e9441bd
Jakub Mazur <kubam0906@gmail.com>
2022-04-02 14:28:18 +0200

    Update master.py

--- master.py
+++ master.py
@@ -1,7 +1,11 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import socket
 
 WIDTH, HEIGHT = 1280, 720
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address_port = ('127.0.0.1', 5052)
 
 
 if __name__ == '__main__':
@@ -16,7 +20,13 @@ if __name__ == '__main__':
         hands, img = detector.findHands(img)
         if hands:
             for hand in hands:
-                ln_list = hand['lnList']
+                lm_list = hand['lmList']
+                data = []
+                for lm in lm_list:
+                    data.extend([lm[0], HEIGHT - lm[1], lm[2]])
+
+                sock.sendto(str.encode(str(data)), server_address_port)
+
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
